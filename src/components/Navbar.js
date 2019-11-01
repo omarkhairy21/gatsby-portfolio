6a2c8e90187cd6743003eb1d57fa91aeef7b9521
Omar Khairy <omarkhairy21@gmail.com>
2019-11-01 23:02:20 +0200

    Change Navbar And Ready To Deploy :construction:

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
 import React from "react"
 import { Link, graphql, useStaticQuery } from "gatsby"
-import { Menu, Image} from 'semantic-ui-react'
+import { Menu, Image, Button} from 'semantic-ui-react'
 import headerStyles from '../styles/header.module.scss';
-//import logo from '../../static/wall.png';
+import logo from '../../static/wall.png';
 const Navbar = () => {
   const data  = useStaticQuery(graphql`
     query {
@@ -16,14 +16,22 @@ const Navbar = () => {
   const StyleMenu = {
     border: 'none',
     display:'flex',
+    margin: '2vh 0'
 
   }
   return (
     <Menu  secondary pointing borderless  style={StyleMenu}>
-        <Menu.Item className={headerStyles.title}>
-        <Link  to="/">
-           { data.site.siteMetadata.title }
-        </Link>
+        <Menu.Item style={{padding: '0 0'}}>
+          <Button basic  animated>
+          <Button.Content hidden inverted>
+          <Link  to="/">
+            { data.site.siteMetadata.title }
+          </Link>
+          </Button.Content>
+          <Button.Content visible >
+            <Image src={logo} />
+          </Button.Content>
+          </Button>
         </Menu.Item>
           <Menu.Item className={headerStyles.navbarLinks}  as='a' position='right' link='true' color='blue'>
            <Link to='/about'>ME</Link>
