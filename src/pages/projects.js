74830fd5d9d7746e404a0ab5615c596050d17ea5
Omar Khairy <omarkhairy21@gmail.com>
2019-11-01 22:15:26 +0200

    Add New Style For Projects Page :art:

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,60 +1,133 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Layout from '../components/layout';
-import {Header, Image, Grid, Segment} from 'semantic-ui-react'
-import projectPicture from '../../static/medium-thumb.png';
-const projects = () => {
-  const br ={
+import {
+  Header, Image,
+  Item, Responsive ,
+  Menu ,Grid,
+  Segment, Button,
+  Icon, Label
+  } from 'semantic-ui-react'
+import projectPicture from '../../static/thumb.png';
+
+class projects extends Component {
+   br = {
     boxSizing: 'border-box',
     height: '0.5rem',
     width: '8rem',
     backgroundColor: '#03a9f4',
     marginBottom: '10vh'
   }
+
+  state = { 
+    activeItem: 'Full Stack',
+    selectedItem:'links'
+  }
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name, selectedItem:name })
+  displaySegment = (name) => (
+    <Segment color='blue' stacked >
+    <Item>
+        <Grid>
+          <Grid.Column width='6'>
+            <Image bordered dimmer rounded src={projectPicture} />
+          </Grid.Column>
+          <Grid.Column width='10'>
+          <Item.Content>
+          <Item.Header as='a'><h2>{name}</h2></Item.Header>
+          <Item.Meta>
+            <span>Date</span>
+            <span>Category</span>
+          </Item.Meta>
+          <Item.Description >
+            A description which may flow for several lines and give context to the content.
+          </Item.Description>
+        </Item.Content>
+          </Grid.Column>
+        </Grid>
+      </Item>
+    </Segment>
+  )
+  menuForMobil = (activeItem) => (
+    <Grid>
+    <Grid.Column width={16}>
+      <Menu attached='top' tabular>
+        <Menu.Item
+          name='Full Stack'
+          active={activeItem === 'Full Stack'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='React.js'
+          active={activeItem === 'React.js'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='TypeScript'
+          active={activeItem === 'TypeScript'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='Algorithms'
+          active={activeItem === 'Algorithms'}
+          onClick={this.handleItemClick}
+        />
+      </Menu>
+    </Grid.Column>
+
+    <Grid.Column stretched width={16}>
+    {this.displaySegment(activeItem)}
+    </Grid.Column>
+  </Grid>
+  )
+
+  menuForPc = (activeItem) => (
+    <Grid>
+    <Grid.Column width={4}>
+      <Menu fluid vertical tabular color='blue'>
+        <Menu.Item
+          name='bio'
+          active={activeItem === 'bio'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='pics'
+          active={activeItem === 'pics'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='companies'
+          active={activeItem === 'companies'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='links'
+          active={activeItem === 'links'}
+          onClick={this.handleItemClick}
+        />
+      </Menu>
+    </Grid.Column>
+
+    <Grid.Column stretched width={12}>
+    {this.displaySegment(activeItem)}
+    </Grid.Column>
+  </Grid>
+  )
+  render(){
+    const { activeItem } = this.state;
     return (
         <Layout>
-              <Header as='h2' color='blue' style={{ marginTop: '10vh' }}>
-               Latest Projects
-            </Header>
-            <hr style={br} />
-            <Grid relaxed >
-            <Segment vertical>  
-            <Grid.Row >
-                <Grid.Column width={4}>
-                  <Image inline src={projectPicture} />
-                </Grid.Column>
-                <Grid.Column>
-                  Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book
-                </Grid.Column>
-              </Grid.Row>
-            </Segment>
-
-              <Grid.Column width={16}>
-                <Segment color='blue'>
-                <Grid>
-                <Grid.Column width={4}>
-                    <Image inline bordered src={projectPicture} />
-                  </Grid.Column>
-                  <Grid.Column width={10}>
-                  <Header>Lorem Title</Header>
-                   <p>
-                   Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book
-                   </p> 
-                  </Grid.Column>
-                </Grid>
-                </Segment>
-              </Grid.Column>
-              <Grid.Column width={4}>
-                <Image src={projectPicture} />
-              </Grid.Column>
-              <Grid.Column width={10}>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-               Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book
-              </Grid.Column>
-            </Grid>
+        <Header as='h2' color='blue' style={{ marginTop: '10vh' }}>
+          Latest Projects
+        </Header>
+        <hr style={this.br} />
+          <Responsive minWidth={220} maxWidth={798}>
+            { this.menuForMobil(activeItem) }
+          </Responsive>
+          <Responsive minWidth={799} maxWidth={2559}> 
+            { this.menuForPc(activeItem) }
+          </Responsive>
         </Layout>   
     );
+  }  
 }
 
 export default projects;
