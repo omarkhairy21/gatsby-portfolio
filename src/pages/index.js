2e9487e31108ed085324844f62ff4dc7ae4cd885
omar khairy <omarkhairy21@gmail.com>
2020-03-16 19:57:11 +0200

    change name of file seo.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link, graphql, useStaticQuery } from 'gatsby'
 import Layout from '../components/layout';
-import SEO from '../components/SEO';
+import SEO from '../components/seo';
 import {
     Divider,
     Grid,
